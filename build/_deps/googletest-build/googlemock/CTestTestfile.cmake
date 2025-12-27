ed955d601d24f10326c443a58e891aa58e6246ca
Aki <achi.roziiz@gmail.com>
2025-12-27 22:07:09 +0100

    Remove build directory from remote repo

--- build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/achillerossi/Documents/C++_tests/memory_pool_mngr/build/_deps/googletest-src/googlemock
-# Build directory: /Users/achillerossi/Documents/C++_tests/memory_pool_mngr/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
